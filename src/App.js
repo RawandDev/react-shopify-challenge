8adb77829aa01b2d75e61b36ecec6b48fea9ab32
RawandDev <rawand.200091384037@gmail.com>
2021-08-24 20:55:20 +0300

    add husky

--- src/App.js
+++ src/App.js
@@ -2,8 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 
+let x;
+
 function App() {
   return (
+    
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
