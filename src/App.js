a077191a010215db53dd8ba55674f9a8bf7c70f2
RawandDev <rawand.200091384037@gmail.com>
2021-08-25 01:19:29 +0300

    add queryHandler

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React from "react";
+import useAxios from "./components/hooks/useAxios";
+import Search from "./components/Search";
 
 function App() {
+  const { response, loading, setQuery } = useAxios();
+
+  function queryHandler(q) {
+    setQuery(q);
+  }
+
+  console.log(response, loading);
   return (
     <div>
+      <Search queryHandler={queryHandler} />
       <h1 className="bg-green-400">Hello</h1>
     </div>
   );
