7ed0206a79927e4d4028575fe12126934d1f6971
RawandDev <rawand.200091384037@gmail.com>
2021-08-25 12:40:03 +0300

    add loadingHandler

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
 import React from "react";
 import useAxios from "./components/hooks/useAxios";
+import MoviesList from "./components/MoviesList";
 import Search from "./components/Search";
 
 function App() {
-  const { response, loading, setQuery } = useAxios();
+  const { response, loading, setQuery, setLoading } = useAxios();
 
   function queryHandler(q) {
     setQuery(q);
   }
 
+  function loadingHandler(l) {
+    setLoading(l);
+  }
+
+  console.log(typeof response);
+
   console.log(response, loading);
   return (
     <div>
-      <Search queryHandler={queryHandler} />
-      <h1 className="bg-green-400">Hello</h1>
+      <Search queryHandler={queryHandler} loadingHandler={loadingHandler} />
+      <MoviesList data={response} loading={loading} />
     </div>
   );
 }
