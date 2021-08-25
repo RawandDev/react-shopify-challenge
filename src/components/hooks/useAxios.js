6a7ae6665dd0602bf7dda5d4f9754ccc5821f879
RawandDev <rawand.200091384037@gmail.com>
2021-08-25 03:17:29 +0300

    fix loading while searching with query

--- src/components/hooks/useAxios.js
+++ src/components/hooks/useAxios.js
@@ -1,17 +1,16 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 
-// const baseURL = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_API_KEY}&query=${query}`;
-
 const useAxios = (axiosParams) => {
   const [response, setResponse] = useState(undefined);
   const [error, setError] = useState("");
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
   const [query, setQuery] = useState("");
 
   const baseURL = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_API_KEY}&query=${query}`;
   axios.defaults.baseURL = `${baseURL}`;
   const fetchData = async (params) => {
+    setLoading(true);
     try {
       const result = await axios.request(params);
       setResponse(result.data);
@@ -26,7 +25,7 @@ const useAxios = (axiosParams) => {
     fetchData(axiosParams);
   }, [axiosParams, query]); // execute when query changes
 
-  return { response, error, loading, query, setQuery };
+  return { response, error, loading, setQuery, setLoading };
 };
 
 export default useAxios;
