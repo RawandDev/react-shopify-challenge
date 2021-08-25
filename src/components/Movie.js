41c2c784cc251d39fdb418397ea37701ec1ac977
RawandDev <rawand.200091384037@gmail.com>
2021-08-25 17:18:37 +0300

    add some rules for displaying movies

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -6,17 +6,28 @@ function Movie({ vote, title, poster, releaseDate }) {
 
   return (
     <div className="float-left p-4 text-white bg-screen ">
-      <div data-aos="flip-left" className=" ">
-        <h2 className="text-center">{title}</h2>
-
-        <img
-          src={`https://image.tmdb.org/t/p/w500/${poster}`}
-          alt={title}
-          className="w-48 rounded-xl"
-        />
-        <p className="text-center">{releaseDate}</p>
-        <p className="text-center">{vote}</p>
-      </div>
+      {vote ? (
+        <div data-aos="flip-left" className=" ">
+          <h2 className="text-center">{title}</h2>
+          {poster ? (
+            <img
+              src={`https://image.tmdb.org/t/p/w500/${poster}`}
+              alt={title}
+              className="w-48 rounded-xl"
+            />
+          ) : (
+            <img
+              className="w-48 rounded-xl"
+              src="https://motivatevalmorgan.com/wp-content/uploads/2016/06/default-movie-1-3.jpg"
+              alt="default"
+            />
+          )}
+          <p className="text-center">{releaseDate}</p>
+          <p className="text-center">{vote}</p>
+        </div>
+      ) : (
+        ""
+      )}
     </div>
   );
 }
