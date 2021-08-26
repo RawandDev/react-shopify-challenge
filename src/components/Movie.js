72228bfc71e02b43cfb01050c830509ee41d6d96
RawandDev <rawand.200091384037@gmail.com>
2021-08-26 21:49:14 +0300

    add nomination button

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,25 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "./UI/Button";
 
 /* eslint-disable react/prop-types */
 function Movie({ vote, title, poster, releaseDate }) {
+  // const [nominated, setNominated] = useState(false);
+  const [isDisabled, setIsDisabled] = useState(false);
+  const [nominateCounter, setNominateCounter] = useState(0);
+
+  function handleNomination() {
+    // setNominated(true);
+    setIsDisabled(true);
+    setNominateCounter((prev) => prev + 1);
+  }
+
   return (
     <div
       className={`${
-        poster || vote !== 0 || title || releaseDate ? "w-48 m-8" : ""
+        poster || vote !== 0 || title || releaseDate
+          ? "w-48 m-8 bg-gray-300 rounded-2xl"
+          : ""
       }`}
     >
       <div>
-        <h2>{title}</h2>
         <img
           className="rounded-2xl"
           src={`https://image.tmdb.org/t/p/w500/${poster}`}
           alt={title}
         />
+        <h2 className={`${title.length > 15 ? "text-sm" : "text-lg"}`}>
+          {title}
+        </h2>
         <p>{releaseDate}</p>
-        <p>{vote}</p>
-        <Button>
+        <p
+          className={`${
+            +vote > 5
+              ? "bg-green-300 w-8 rounded-lg"
+              : "bg-red-300 w-8 rounded-lg"
+          }`}
+        >
+          {vote}
+        </p>
+        <Button onClick={handleNomination} disabled={isDisabled}>
           <p>Nominate</p>
+          {nominateCounter}
         </Button>
       </div>
     </div>
