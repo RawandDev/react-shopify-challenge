168fe97a9326b21d3144fd98f49ba4bc026b26e8
RawandDev <rawand.200091384037@gmail.com>
2021-08-26 16:53:56 +0300

    refactor Search.js

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
 import PropTypes from "prop-types";
+import SubmitButton from "./UI/SubmitButton";
+import Input from "./UI/Input";
 
 function Search({ queryHandler, loadingHandler }) {
   const [queryValue, setQueryValue] = useState("");
-  //   const [loading, setLoading] = useState(true);
 
   function handleChange(e) {
     setQueryValue(e.target.value);
@@ -26,18 +27,20 @@ function Search({ queryHandler, loadingHandler }) {
       className=" text-center"
       data-aos="fade-right"
     >
-      <input
-        className="border-4 rounded-xl w-1/2 h-10 bg-transparent focus:outline-none hover: text-white p-2   "
-        type="text"
-        placeholder="Search for a movie"
-        onChange={handleChange}
-      />
-      <button
-        type="submit"
-        className="bg-transparent m-6 border-4 rounded-xl w-20 h-10 text-white hover:bg-red-800  "
-      >
-        Search
-      </button>
+      <div className="flex justify-center items-center">
+        <div className="relative">
+          <Input
+            type="text"
+            className="h-14 w-96 pl-10 pr-20 rounded-lg z-0 focus:shadow focus:outline-none"
+            placeholder="Search anything..."
+            onChange={handleChange}
+          />
+
+          <div className="absolute top-2 right-2">
+            <SubmitButton>Search</SubmitButton>
+          </div>
+        </div>
+      </div>
     </form>
   );
 }
