5ba1542713b2e4424bca1985398aaf407898d58d
RawandDev <rawand.200091384037@gmail.com>
2021-08-26 21:52:16 +0300

    add disable nomination

--- src/components/UI/Button.js
+++ src/components/UI/Button.js
@@ -1,18 +1,26 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-function Button({ children }) {
+function Button({ children, onClick, disabled }) {
   // this is proptypes for children
   Button.propTypes = {
     children: PropTypes.oneOfType([
       PropTypes.arrayOf(PropTypes.node),
       PropTypes.node,
     ]).isRequired,
+    onClick: PropTypes.func.isRequired,
+    disabled: PropTypes.bool.isRequired,
   };
   return (
     <button
+      onClick={onClick}
+      disabled={disabled}
       type="button"
-      className="inline-flex items-center justify-center w-32 h-10 mr-2 text-indigo-100 transition-colors duration-150 bg-green-500 rounded-lg focus:shadow-outline hover:bg-green-900"
+      className={`${
+        !disabled
+          ? "inline-flex items-center justify-center w-32 h-10 mr-2 text-indigo-100 transition-colors duration-150 bg-green-500 rounded-lg focus:shadow-outline hover:bg-green-900"
+          : "inline-flex items-center justify-center w-32 h-10 mr-2 text-indigo-100 transition-colors duration-150 bg-gray-500 rounded-lg focus:shadow-outline cursor-default"
+      }`}
     >
       <svg className="w-4 h-4 fill-current" viewBox="0 0 20 20">
         <path
