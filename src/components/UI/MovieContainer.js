bb750d1dd814f9928efa387f48aae120db2b03a5
RawandDev <rawand.200091384037@gmail.com>
2021-08-26 21:46:26 +0300

    adjust font size

--- src/components/UI/MovieContainer.js
+++ src/components/UI/MovieContainer.js
@@ -11,7 +11,7 @@ function MovieContainer({ children }) {
   };
 
   return (
-    <div className="flex items-center justify-center flex-row flex-wrap text-black font-bold text-center text-lg">
+    <div className="flex items-center justify-center flex-row flex-wrap text-black font-bold text-center">
       {children}
     </div>
   );
