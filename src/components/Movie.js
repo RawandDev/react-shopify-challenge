8894f48cce87a2b86222bb8e58b818805519a62e
RawandDev <rawand.200091384037@gmail.com>
2021-08-26 16:52:47 +0300

    refactor Movie.js

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,33 +1,27 @@
 import React from "react";
+import Button from "./UI/Button";
 
 /* eslint-disable react/prop-types */
 function Movie({ vote, title, poster, releaseDate }) {
-  console.log(vote, title, poster, releaseDate);
-
   return (
-    <div className="float-left p-4 text-white bg-screen ">
-      {vote ? (
-        <div data-aos="flip-left" className=" ">
-          <h2 className="text-center">{title}</h2>
-          {poster ? (
-            <img
-              src={`https://image.tmdb.org/t/p/w500/${poster}`}
-              alt={title}
-              className="w-48 rounded-xl"
-            />
-          ) : (
-            <img
-              className="w-48 rounded-xl"
-              src="https://motivatevalmorgan.com/wp-content/uploads/2016/06/default-movie-1-3.jpg"
-              alt="default"
-            />
-          )}
-          <p className="text-center">{releaseDate}</p>
-          <p className="text-center">{vote}</p>
-        </div>
-      ) : (
-        ""
-      )}
+    <div
+      className={`${
+        poster || vote !== 0 || title || releaseDate ? "w-48 m-8" : ""
+      }`}
+    >
+      <div>
+        <h2>{title}</h2>
+        <img
+          className="rounded-2xl"
+          src={`https://image.tmdb.org/t/p/w500/${poster}`}
+          alt={title}
+        />
+        <p>{releaseDate}</p>
+        <p>{vote}</p>
+        <Button>
+          <p>Nominate</p>
+        </Button>
+      </div>
     </div>
   );
 }
